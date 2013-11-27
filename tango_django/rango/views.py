c367df7492eeb693fc9df413ad24fa7736fe1795
amasri <amir.masri@coface.com>
2013-11-27 13:37:01 -0500

    Complete chapter 8

--- tango_django/rango/views.py
+++ tango_django/rango/views.py
@@ -3,6 +3,9 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -111,3 +114,36 @@ def register(request):
         profile_form = UserProfileForm()
 
     return render_to_response('rango/register.html', {'user_form': user_form, 'profile_form': profile_form, 'registered': registered}, context)
+
+
+def user_login(request):
+    context = RequestContext(request)
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/rango/')
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            print "Invalid login details: {0}, {1}".format(username, password)
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render_to_response('rango/login.html', {}, context)
+
+
+@login_required
+def restricted(request):
+    return HttpResponse("You are logged in!")
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/rango/')
